91e39ad017fc3ef1f9f32fe5b2eba8eb96ec72c1
Rami Zeid <ra.zein.zein@gmail.com>
2021-06-27 02:54:09 -0400

    Pushing everything

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-### Website link: https://ramizeid.github.io/rami-zeid-seg3525-lab4/
+### Website link: https://ramizeid.github.io/rami-zeid-seg3525-lab5/
+"# -rami-zeid-seg3525-lab5" 
--- scripts/main.js
+++ scripts/main.js
@@ -25,25 +25,88 @@ function toTopScroll() {
 }
 
 function landingArrowScroll() {
+    const currentUrl = window.location.href.split("#")[0];
+
     document.getElementById('about').scrollIntoView({
         behavior: 'smooth'
     })
+
+    window.history.pushState("", "/services", `${currentUrl}#about`);
 }
 
 function therapistsScroll() {
+    const currentUrl = window.location.href.split("#")[0];
+
     document.getElementById('therapists').scrollIntoView({
         behavior: 'smooth'
     })
+
+    window.history.pushState("", "/services", `${currentUrl}#therapists`);
 }
 
 function servicesScroll() {
+    const currentUrl = window.location.href.split("#")[0];
+
     document.getElementById('services').scrollIntoView({
         behavior: 'smooth'
     })
+
+    window.history.pushState("", "/services", `${currentUrl}#services`);
 }
 
 function bookingScroll() {
+    const currentUrl = window.location.href.split("#")[0];
+
     document.getElementById('booking').scrollIntoView({
         behavior: 'smooth'
     })
+
+    window.history.pushState("", "/services", `${currentUrl}#booking`);
+}
+
+function therapistChange() {
+    const dropdown = $('#q3-dropdown');
+    const datepicker = $("#q6-datepicker");
+    const date = new Date();
+	const today = new Date(date.getFullYear(), date.getMonth(), date.getDate());
+
+    datepicker.datepicker("destroy");
+
+    if(dropdown.val() === "q3-1") {
+        datepicker.datepicker({
+            format: "dd.mm.yyyy",
+            todayBtn: "linked",
+            minDate: today,
+            language: "en",
+
+            beforeShowDay: function (d) {
+                const day = d.getDay();
+                return [(day !== 0 && day !== 2 && day !== 4 && day !== 5 && day !== 6)];
+            }
+        })
+    } else if (dropdown.val() === "q3-2") {
+        datepicker.datepicker({
+            format: "dd.mm.yyyy",
+            todayBtn: "linked",
+            minDate: today,
+            language: "en",
+
+            beforeShowDay: function (d) {
+                const day = d.getDay();
+                return [(day !== 0 && day !== 1 && day !== 3 && day !== 5 && day !== 6)];
+            }
+        })
+    } else if (dropdown.val() === "q3-3") {
+        datepicker.datepicker({
+            format: "dd.mm.yyyy",
+            todayBtn: "linked",
+            minDate: today,
+            language: "en",
+
+            beforeShowDay: function (d) {
+                const day = d.getDay();
+                return [(day !== 0 && day !== 1 && day !== 2 && day !== 3 && day !== 4 && day !== 6)];
+            }
+        })
+    }
 }
